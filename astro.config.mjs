f2654a4db8a7cc03d3dfa4726903e0a5fae1a9e7
aki-misaki <akiya.digital@gmail.com>
2026-03-24 15:02:45 +0900

    サイトマップ追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // 👇 サイトのURLは、この「波カッコ」の中に書きます！
+  site: 'https://misaki-ikokka.pages.dev',
+  
+  // 👇 サイトマップの機能をONにする設定です
+  integrations: [sitemap()]
+});
